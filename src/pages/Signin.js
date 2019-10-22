5a318e3daf4ebdc4183158b388d33fe6f64085e4
pogayo <pogayo17@alustudent.com>
2019-10-22 21:54:58 +0200

    Secured

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -11,12 +11,12 @@ export default class Signin extends Component {
 
     render() {
         return (
-            <div style={styles} className="bg">
-                <div  className="flex-col-centerAll">
+            <div style={styles} className="bg flex-col-centerAll">
+                <div  className="">
                     <h3>CS Clubs</h3>
-                    <h4>Sign in</h4>
+                    <h5>Sign in</h5>
                     <form>
-                        <input type="email" placeholder="Email"/>
+                        <input type="email" placeholder="Email" required/>
                         <button>Next</button>
                     </form>
                     <p> or</p>
