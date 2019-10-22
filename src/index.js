4ad2242397ac53e7d99ceac3157197e9de5b4e63
pogayo <pogayo17@alustudent.com>
2019-10-22 20:27:41 +0200

    Created project folders

--- src/index.js
+++ src/index.js
@@ -4,9 +4,24 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import * as firebase from 'firebase';
+
+
+var config={
+    apiKey: "AIzaSyA9Axkckj4kk1npt9_kYQXPcaxHqwPbTzU",
+    authDomain: "clubs-info-sys.firebaseapp.com",
+    databaseURL: "https://clubs-info-sys.firebaseio.com",
+    projectId: "clubs-info-sys",
+    storageBucket: "clubs-info-sys.appspot.com",
+    messagingSenderId: "755704539890",
+    appId: "1:755704539890:web:497c5bcabb6169f1a3be6b",
+    measurementId: "G-JYHEH9SXK5"
+};
+
+firebase.initializeApp(config);
+firebase.analytics();
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
