53b48c8103bff9c14e71278babfb4e68cb043b3c
pogayo <pogayo17@alustudent.com>
2019-10-23 04:57:48 +0200

    homepage about to be done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,14 +15,14 @@ export default class Home extends Component{
 
 
    render (){
-       return (<div>
+       return (<div className="flex-col-centerAll">
            <Header/>
-           <h2 style={{fontSize:"3rem"}}>What would you like to do?</h2>
+           <h2 style={{fontSize:"2.5rem" , padding:"0px 20px", textAlign:"center"}}>What would you like to do?</h2>
            <div id="options-container">
                <Link  to="/" className="options-element"><img src={meeting} alt="" className="home-icon" />Create a meeting</Link>
-               <div className="options-element"><img src={schedule} alt="" className="home-icon" />Schedule a meeting</div>
-               <div className="options-element"><img src={attendance} alt="" className="home-icon" />Take attendance</div>
-               <div className="options-element"><img src={records} alt="" className="home-icon" /> View Records</div>
+               <Link to="/" className="options-element"><img src={schedule} alt="" className="home-icon" />Schedule a meeting</Link>
+               <Link  to="/" className="options-element"><img src={attendance} alt="" className="home-icon" />Take attendance</Link>
+               <Link  to="/" className="options-element"><img src={records} alt="" className="home-icon" /> View Records</Link>
 
            </div>
        </div>);
