bcb098aba5129ca136fc7c882d2477d2713d63d1
pogayo <pogayo17@alustudent.com>
2019-12-18 20:37:13 +0200

    No clean up, validation and role  based access

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -7,18 +7,16 @@ export default class Create extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      formValues: {
-        title: "",
-        purpose: "",
-        date: "",
-        eventType: "",
-        startTime: "13:00",
-        endTime: "13:00",
-        venue: "",
-      },
+      title: "",
+      purpose: "",
+      date: "",
+      eventType: "",
+      startTime: "13:00",
+      endTime: "13:00",
+      venue: "",
       message: "",
       validFields: [],
-      formValid: false
+      formValid: true
     }
   }
 
@@ -28,24 +26,22 @@ export default class Create extends Component {
     switch (field) {
       case "date":
         const date = new Date();
-        const dateString = date.getMonth + "/" + date.getDate + "/" + date.getFullYear;
-        if (dateString > this.state.formValues.date) {
+        const dateString = formatDate(date)
+        if (dateString > this.state.date) {
           return [1, "You are late"];
         }
+      
         break
       case "startTime":
-        if (this.state.formValues.startTime < currentHour - 24) {
-          return [0, "You are late"];
-        }
-        break;
+         break;
       case "endTime":
-        if (this.state.formValues.endTime <= this.state.startTime) {
+        if (this.state.endTime <= this.state.startTime) {
           this.setState({ formValid: false })
           return [-1, "End time should be greater than your start time"];
         }
-        if (this.state.formValues.endTime < currentHour - 24) {
-          return [0, "You are late"];
-        }
+        // if (this.state.formValues.endTime < currentHour - 24) {
+        //   return [0, "You are late"];
+        // }
         break;
       default:
         break;
@@ -62,16 +58,16 @@ export default class Create extends Component {
     this.setState({ formValid: true });
   }
 
-  
-  handleChange = (e) => {
-    const field = e.target.name;
-    const value = e.target.value
-     console.log(value)
-    this.setState({ ["formValues."+field]: value },
-      this.validateField(e, field, value));
 
-    console.log(this.state.formValues[e.target.name]);
-    this.validateForm();
+  handleChange = (e) => {
+    const field=e.target.name;
+      const value=e.target.value
+     
+     this.setState({[field]:value},
+       this.validateField(e,field,value));
+ 
+     console.log(this.state[e.target.name]);
+     //this.validateForm();
 
   }
 
@@ -81,9 +77,19 @@ export default class Create extends Component {
     const dbRef = firebase.firestore().collection("meetings");
     if (this.state.formValid) {
       e.preventDefault();
+      let state = this.state;
       console.log("Got here")
+      let formValues = {
+        title: state.title,
+        purpose: state.purpose,
+        date: state.date,
+        eventType: state.eventType,
+        startTime: state.startTime,
+        endTime: state.endTime,
+        venue: state.venue,
+      }
       dbRef.add(
-        this.state).then((res) => {
+        formValues).then((res) => {
 
           this.setState({ message: "Your entry was successfuly submitted" })
 
@@ -95,8 +101,8 @@ export default class Create extends Component {
         );
     }
 
-  
-}
+
+  }
   render() {
 
     return (
@@ -104,31 +110,44 @@ export default class Create extends Component {
         <Header />
         <div id="form-container" className="flex-col-centerAll">
 
-          <div>{this.state.message}</div>
-          <form onSubmit={this.handleSubmit} className="flex-col-centerAll" id="create-form">
-            <input value={this.state.formValues.title} name="title" type="text" onChange={this.handleChange} placeholder="Event title" required />
-            <input value={this.state.formValues.purpose} name="purpose" type="text" onChange={this.handleChange} placeholder="Event Purpose" required />
-            <select style={{ width: "100%" }}>
-              <option>Type of meeting</option>
-              <option>Weekly meetup</option>
-              <option>Leaders meeting</option>
-              <option>Leaders meeting with patron</option>
-              <option>Field visit/Activity</option>
-              <option>Internal exhibition</option>
-              <option>External exhibition</option>
-              <option>Other</option>
+          <div style={{color:"green", paddingBottom:"5px"}}>{this.state.message}</div>
+          <form  onSubmit={this.handleSubmit} className="flex-col-centerAll" id="create-form">
+      <input value={this.state.title} name="title" type="text" onChange={this.handleChange} placeholder="Event title" required/>
+      <input value={this.state.purpose} name="purpose" type="text" onChange={this.handleChange} placeholder="Event Purpose" required/>
+      <select  style={{width:"100%"}}name="eventType" onChange={this.handleChange} required>
+        <option>Type of meeting</option>
+        <option>Weekly meetup</option>
+        <option>Leaders meeting without patron</option>
+        <option>Leaders meeting with patron</option>
+        <option>Field visit/Activity</option>
+        <option>Internal exhibition</option>
+        <option>External exhibition</option>
+        <option>Other</option>
 
-            </select>
-            <input value={this.state.formValues.date} name="date" type="date" onChange={this.handleChange} placeholder="Date" required />
-            Start time
-      <input value={this.state.formValues.startTime} placeholder="Start time" name="startTime" type="time" onChange={this.handleChange} required />
-            End time
-      <input value={this.state.formValues.endTime} name="endTime" type="time" onChange={this.handleChange} placeholder="End time" required />
-            <input value={this.state.formValues.venue} name="venue" type="text" onChange={this.handleChange} placeholder="Venue" required />
-            <button type="submit" className="submit-btn" disabled={!this.state.formValid} onClick={this.handleSubmit}>Submit</button>
-          </form>
+      </select>
+      <input value={this.state.date} name="date" type="date" onChange={this.handleChange} placeholder ="Date" required/>
+      Start time
+      <input value={this.state.startTime} placeholder="Start time" name="startTime" type="time" onChange={this.handleChange} placeholder ="Start time" required/>
+      End time
+      <input value={this.state.endTime} placeholder="End time"name="endTime" type="time" onChange={this.handleChange} placeholder ="End time" required/>
+      <input value={this.state.venue} name="venue" type="text" onChange={this.handleChange} placeholder ="Venue" required/>
+      <button type="submit" className="submit-btn" disabled={!this.state.formValid} onClick={this.handleSubmit}>Submit</button>
+      </form>
         </div>
       </div>)
       ;
   }
+}
+
+function formatDate(date) {
+  var day = date.getDate();
+  if (day < 10) {
+    day = "0" + day;
+  }
+  var month = date.getMonth() + 1;
+  if (month < 10) {
+    month = "0" + month;
+  }
+  var year = date.getFullYear();
+  return year + "-" + month + "-" + day;
 }
